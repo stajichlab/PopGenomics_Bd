f8ffedd94f65163f7a351ca114e4e09cdce07a12
Jason Stajich <jasonstajich.phd@gmail.com>
2020-11-11 19:59:32 -0800

    download scripts

--- pipeline/00_index.sh
+++ pipeline/00_index.sh
@@ -5,10 +5,11 @@ module load picard
 if [ -f config.txt ]; then
 	source config.txt
 fi
+mkdir genome
 pushd genome
 # THIS IS EXAMPLE CODE FOR HOW TO DOWNLOAD DIRECT FROM FUNGIDB
-RELEASE=39
-SPECIES=AfumigatusAf293
+RELEASE=49
+SPECIES=BdendrobatidisJEL423
 URL=https://fungidb.org/common/downloads/release-${RELEASE}/$SPECIES
 PREF=FungiDB-${RELEASE}_${SPECIES}
 FASTAFILE=${PREF}_Genome.fasta
